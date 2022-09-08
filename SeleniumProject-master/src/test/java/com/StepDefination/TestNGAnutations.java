4baef197cee538dcea17f74eaf1af62616cf8d01
Basir <baseer.wardak@gmail.com>
2022-09-07 21:23:36 -0700

    Created TestNG Annotation class

---
+++ SeleniumProject-master/src/test/java/com/StepDefination/TestNGAnutations.java
@@ -0,0 +1,19 @@
+package com.StepDefination;
+
+import org.junit.Test;
+
+public class TestNGAnutations {
+
+	
+	
+	@Test (groups = {"regrissinoTest", "SmokeTest"})
+	public void beforMethod() {
+		
+		
+
+	}
+	
+	
+	
+
+}
